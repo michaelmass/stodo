919a3f093373c58cfe1b08f0ccd9f200eb4b6e56
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2024-04-12 16:49:31 -0400

    chore(ci): added ci to lint & publish the package (#8)

---
+++ ci/publish.ts
@@ -0,0 +1,20 @@
+import { getIDToken } from 'npm:@actions/core'
+import { fmt, lint } from 'https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/biome.ts'
+import { connect } from 'https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/dagger.ts'
+import { check, publish } from 'https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/deno.ts'
+
+await connect(async client => {
+  const params = {
+    client,
+    dir: client.host().directory('.'),
+  }
+
+  await lint({ client })
+  await fmt({ client })
+  await check({ client, entrypoints: ['src/mod.ts', 'src/cli.ts'] })
+
+  const githubToken = await getIDToken()
+  const githubTokenSecret = client.setSecret('GITHUB_OIDC_TOKEN', githubToken)
+
+  await publish({ ...params, token: githubTokenSecret })
+})

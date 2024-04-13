b964524ca45a165f617090f73959bd1e3b1a6037
michaelmass <10071388+michaelmass@users.noreply.github.com>
2024-04-13 13:27:11 -0400

    update

--- ci/publish.ts
+++ ci/publish.ts
@@ -1,13 +1,8 @@
-import { getIDToken } from 'npm:@actions/core'
 import { connect } from 'https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/dagger.ts'
 import { publish } from 'https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/deno.ts'
 import { lint } from './util.ts'
 
 await connect(async client => {
   await lint(client)
-
-  const githubToken = await getIDToken()
-  const githubTokenSecret = client.setSecret('GITHUB_OIDC_TOKEN', githubToken)
-
-  await publish({ client, token: githubTokenSecret })
+  await publish({ client })
 })
